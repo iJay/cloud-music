b56ed44b074557a21b871b377d4791d3bae57663
hanbing2022 <hblx2017@163.com>
2024-02-01 10:51:14 +0800

    引入并配置router && redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { GlobalBasicStyle } from "./style";
 import { IconStyle } from "./assets/iconfont/iconfont";
+// renderRoutes 读取路由配置转化为 Route 标签
+import { renderRoutes } from 'react-router-config';
+import { HashRouter } from 'react-router-dom';
+import routes from './routes/index.js';
+
+import { Provider } from 'react-redux';
+import store from "./store/index.js";
 function App() {
   return (
-    <div className="app">
+    <Provider store={store}>
+      <HashRouter>
       <GlobalBasicStyle />
       <IconStyle />
-      <h3>Hello World!<i className="iconfont">&#xe62b;</i></h3>
-    </div>
+      {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
---
+++ src/store/reducer.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+export default combineReducers({
+  // 开发具体功能模块的时候添加 reducer
+});
