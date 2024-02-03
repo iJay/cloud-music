ad03135342cd56f85e59c490dce5948f1c0f0310
iJay <hblx2017@163.com>
2024-02-03 22:59:33 +0800

    feat: 歌手页面 列表样式和mock数据

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Horization from '../../baseUI/horization-item';
-import { NavContainer } from "./style";
-import { categoryTypes, alphaTypes } from '../../api/config';
+import React, { useState } from 'react'
+import Horization from '../../baseUI/horization-item'
+import Scroll from '../../components/scroll'
+import { NavContainer, List, ListItem, ListContainer } from "./style"
+import { categoryTypes, alphaTypes } from '../../api/config'
 
 function Singers (props) {
   let [category, setCategory] = useState('')
@@ -13,21 +14,57 @@ function Singers (props) {
   const handleUpdateCategory = (value) => {
     setCategory(value)
   }
+
+  //mock 数据
+  const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map (item => {
+    return {
+      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+      name: "隔壁老樊",
+      accountId: 277313426,
+    }
+  });
+
+  const renderSingerList = () => {
+    return (
+      <List>
+        {
+          singerList.map((item, index) => {
+            return (
+              <ListItem key={item.accountId+""+index}>
+                <div className="img_wrapper">
+                  <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+                </div>
+                <span className="name">{item.name}</span>
+              </ListItem>
+            )
+          })
+        }
+      </List>
+    )
+  }
+
   return (
-    <NavContainer>
-      <Horization 
-        list={categoryTypes} 
-        title={"分类 (默认热门):"}
-        oldVal={ category }
-        handleClick={ handleUpdateCategory }
-      ></Horization>
-      <Horization
-        list={alphaTypes}
-        title={"首字母:"}
-        oldVal={ alpha }
-        handleClick={ handleUpdateAlpha }
-      ></Horization>
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <Horization 
+          list={categoryTypes} 
+          title={"分类 (默认热门):"}
+          oldVal={ category }
+          handleClick={ handleUpdateCategory }
+        ></Horization>
+        <Horization
+          list={alphaTypes}
+          title={"首字母:"}
+          oldVal={ alpha }
+          handleClick={ handleUpdateAlpha }
+        ></Horization>
+      </NavContainer>
+      <ListContainer>
+        <Scroll>
+          { renderSingerList () }
+        </Scroll>
+      </ListContainer>
+    </div>
   )
 }
 
